85d10798ef1bb6d96e6d413def7a4561e70505a6
Markus Wind <markuswind@gmail.com>
2016-07-03 14:08:53 +0200

    added macvim config files

--- .setup-symlinks.sh
+++ .setup-symlinks.sh
@@ -4,7 +4,7 @@ dir=~/dotfiles
 cd $dir
 
 YELLOW = '\033[0;31m'
-files="bash_aliases gitconfig githelpers tmux.conf zshrc xvimrc"
+files="bash_aliases gitconfig githelpers tmux.conf zshrc vimrc gvimrc xvimrc"
 
 for file in $files;
 do
@@ -15,3 +15,7 @@ done
 echo "Creating symlink to .config/nvim folder"
 mkdir -p $dir/.config
 ln -sf $dir/.config/nvim ~/.config/nvim
+
+echo "Creating symlink to .vim folder"
+mkdir -p $dir/.vim
+ln -sf $dir/.vim ~/.vim
