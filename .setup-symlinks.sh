1ef18b64c243c1a09ff55b915c1da84e0036088e
Markus Wind <markuswind@gmail.com>
2016-07-03 14:38:34 +0200

    stopped using neovim

--- .setup-symlinks.sh
+++ .setup-symlinks.sh
@@ -4,17 +4,13 @@ dir=~/dotfiles
 cd $dir
 
 YELLOW = '\033[0;31m'
-files="bash_aliases gitconfig githelpers tmux.conf zshrc vimrc gvimrc xvimrc"
+files="bash_aliases gitconfig githelpers zshrc vimrc xvimrc"
 
 for file in $files;
 do
    echo "Creating symlink to $file in home directory"
    ln -sf $dir/.$file ~/.$file
 done
-
-echo "Creating symlink to .config/nvim folder"
-mkdir -p $dir/.config
-ln -sf $dir/.config/nvim ~/.config/nvim
 
 echo "Creating symlink to .vim folder"
 mkdir -p $dir/.vim
