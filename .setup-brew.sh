b5c217af5e1a3057203c5e4fc1ed9657b9563bb7
Markus Wind <markuswind@gmail.com>
2016-09-08 10:14:08 +0200

    updated brewfile

--- .setup-brew.sh
+++ .setup-brew.sh
@@ -1,11 +1,15 @@
 brew update
 brew upgrade
 
+brew install android-sdk
 brew install carthage
 brew install zsh-syntax-highlighting
 brew install mercurial
+brew install node
 brew install openssl
 brew install wget
+brew install redis
+brew install vim
 brew install zsh-syntax-highlighting
 
 # react native libraries
@@ -17,11 +21,18 @@ nvm install iojs
 nvm alias default iojs
 npm install -g react-native-cli
 
+brew cask install alfred
+brew cask install android-studio
 brew cask install dropbox
 brew cask install flux
 brew cask install gimp
 brew cask install google-chrome
 brew cask install iterm2
+brew cask install java
 brew cask install slack
 brew cask install telegram
 brew cask install transmission
+
+# gems
+gem install xcpretty
+gem install react-native-logcat
