ebaefa2178410f42adbea92887cf4c47ac677181
Markus Wind <markuswind@gmail.com>
2016-09-09 16:41:11 +0200

    add atom config files + atom setup script

---
+++ .setup-atom.sh
@@ -0,0 +1,17 @@
+brew cask install atom
+
+apm install atom-react-native-autocomplete
+apm install base16-ocean-dark-syntax-theme
+apm install git-blame
+apm install highlight-line
+apm install highlight-selected
+apm install language-babel
+apm install language-javascript-jsx
+apm install linter
+apm install linter-eslint
+apm install nuclide
+apm install prime-file-icons
+apm install set-syntax
+apm install vim-mode
+
+ln -sf ~/dotfiles/.atom ~/
