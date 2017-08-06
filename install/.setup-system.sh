fcc381bda99838ce16d1945dfc19625509c4eeee
Markus Wind <markuswind@gmail.com>
2017-08-06 13:36:47 +0200

    removed atom setup + renamed theme file

--- install/.setup-system.sh
+++ install/.setup-system.sh
@@ -1,7 +1,3 @@
-# MANUAL INSTALLATION::
-# xcode
-# macvim - https://github.com/alloy/macvim/wiki/Building
-
 # install & setup brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 sh .setup-brew.sh
@@ -9,9 +5,6 @@ sh .setup-brew.sh
 # install oh-my-zsh & setup symlinks
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sh .setup-symlinks.sh
-
-# install atom
-sh .setup-atom.sh
 
 # setup osx settings
 sh .setup-osx.sh
