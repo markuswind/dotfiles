cf740dab523c4a0cc9e56481785b3f80ef3377cc
Markus Wind <markuswind@gmail.com>
2017-08-15 23:09:41 +0200

    added iterm2 config symlink

--- install/.setup-symlinks.sh
+++ install/.setup-symlinks.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 dir=~/dotfiles
-cd $dir
+cd $dir || exit
 
 files="bash_aliases gitconfig githelpers zshrc vimrc"
 
 for file in $files;
 do
    echo "Creating symlink to $file in home directory"
-   ln -sf $dir/.$file ~/.$file
+   ln -sf $dir/."$file" ~/."$file"
 done
 
 echo "Creating symlink to .config/nvim folder"
@@ -16,6 +16,9 @@ ln -sf $dir/.config/nvim ~/.config/nvim
 
 echo "Creating symlink to .config/htop folder"
 ln -sf $dir/.config/htop ~/.config/htop
+
+echo "Creating symlink to .config/iterm2 folder"
+ln -sf $dir/.config/iterm2 ~/.config/iterm2
 
 echo "Creating symlink for wind.zsh-theme"
 ln -sf ~/dotfiles/.oh-my-zsh/custom/themes/wind.zsh-theme ~/.oh-my-zsh/custom/themes/wind.zsh-theme
