c7cc7b2d1b64d858acfb2b902c28dd85bed94301
Markus Wind <markuswind@gmail.com>
2018-08-23 10:07:53 +0200

    added ngrok

--- install/.setup-brew.sh
+++ install/.setup-brew.sh
@@ -29,6 +29,7 @@ brew cask install godot
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install java
+brew cask install ngrok
 brew cask install slack
 brew cask install spotify
 brew cask install telegram
