c95211e9d7598ece03c853b78dbc7c34e82404dc
Markus Wind <markuswind@gmail.com>
2019-09-02 09:39:11 +0200

    added sshfs brew

--- install/.setup-brew.sh
+++ install/.setup-brew.sh
@@ -14,6 +14,7 @@ brew install python3
 brew install pidcat
 brew install ripgrep
 brew install shellcheck
+brew install sshfs
 brew install thefuck
 brew install tig
 brew install tree
@@ -34,6 +35,7 @@ brew cask install google-chrome
 brew cask install iterm2
 brew cask install java
 brew cask install ngrok
+brew cask install osxfuse # needed for sshfs
 brew cask install sequel-pro
 brew cask install slack
 brew cask install spotify
